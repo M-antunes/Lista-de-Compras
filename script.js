23817af5de46ef054d2948a74788d7c1afdd5556
M-antunes <78101680+M-antunes@users.noreply.github.com>
2021-02-10 09:00:38 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,35 +1,43 @@
 const inputItem = document.querySelector('#input1');
 const inputQuantity = document.querySelector('#input2');
+const inputType = document.querySelector('#input3');
 const addToList = document.querySelector('#btn');
 const msg = document.querySelector('#msg');
 const list = document.querySelector('#list');
 
 
-function itemRemove(btnRemove) {
-    let listItem = document.querySelector('#list');
-     listItem.parentNode.removeChild(listItem);
- 
- }
+function showError(message) {
+    msg.innerHTML = message;
+    setTimeout(() => msg.innerHTML = '', 4000);
+}
 
-
-
+   
 addToList.addEventListener('click', (e) => {
    
-    if(inputItem.value === '' || inputQuantity.value === '') {
-        msg.classList.add('msg');
-        msg.innerHTML = 'Adicione um item';
-        setTimeout(() => msg.remove(), 4000);
+    if(inputItem.value === '') { 
+        showError('Adicionar um Item');
+        return;
     }
-    else {
-       let li = document.createElement('li');
-       li.appendChild(document.createTextNode(`${inputItem.value} :  ${inputQuantity.value}`))
-
-       list.appendChild(li);
-    
-        inputItem.value = '';
-        inputQuantity.value = '';
+    if(inputQuantity.value === '') {
+        showError('Adicionar uma Quantidade');
+        return;
+    } 
+    if(inputType.value === '') {
+       showError('Adicionar um Tipo');
+       return;
     }
+   
+    let li = document.createElement('li');
+    li.appendChild(document.createTextNode(`${inputItem.value} :  ${inputQuantity.value} ${inputType.value}`))
 
-    
- 
+    li.addEventListener('click', (e) => {
+        e.target.remove();
+    })
+
+    list.appendChild(li);
+
+    inputItem.value = '';
+    inputQuantity.value = '';
+    inputType.value = '';
+
 })
